147f4722b87632a5c95f6af304fe6399e0103adf
shelag <pacini.sara@gmail.com>
2015-06-16 17:51:21 +0200

    fixed import paths

--- saving/api.py
+++ saving/api.py
@@ -1,47 +1,35 @@
-
-import sys
-
 class TextError(Exception):
-	pass
+    pass
+
+
 class AmountError(Exception):
-	pass
+    pass
+
 
 def parz_mov(line):
-	""" return word and import of a string line"""
-	amount = None
-	text = []
-	line = line.split(" ")
-	for token in line:
-		try:
-			amount = float(token)	
-		except ValueError:		
-			if token != 'EUR':
-				text.append(token)			
-	print(amount)	
-	if len(text) == 0: #or len(amount) == 0:
-		raise TextError('Errore! non hai inserito correttamente il testo ')
+    """ return word and import of a string line"""
+    amount = None
+    text = []
+    line = line.split(" ")
+    for token in line:
+        try:
+            amount = float(token)
+        except ValueError:
+            if token != 'EUR':
+                text.append(token)
+    print(amount)
+    if len(text) == 0:  # or len(amount) == 0:
+        raise TextError('Errore! non hai inserito correttamente il testo ')
 
-	if amount is None:
-		raise AmountError('Errore! non hai inserito correttamente l\'importo')
+    if amount is None:
+        raise AmountError('Errore! non hai inserito correttamente l\'importo')
 
-	return text, amount
-	
-	
+    return text, amount
+
+
 def test():
-	print('Running tests . . . ')
-	assert parz_mov('enel 30.5 EUR') == (['enel'], 30.5)
-	assert parz_mov('enel EUR 30.5') == (['enel'], 30.5)
-	assert parz_mov('enel 30.5 elettricita') == (['enel', 'elettricita'], 30.5)
-	print ('All tests ok!')
-
-if __name__ == "__main__":
-
-    if len(sys.argv) == 1:
-        test()
-        sys.exit(0)
-
-
-    in_file = sys.argv[1]
-
-
-    		
+    print('Running tests . . . ')
+    assert parz_mov('enel 30.5 EUR') == (['enel'], 30.5)
+    assert parz_mov('enel EUR 30.5') == (['enel'], 30.5)
+    assert parz_mov('enel 30.5 elettricita') == (['enel', 'elettricita'], 30.5)
+    print('All tests ok!')
--- saving/models.py
+++ saving/models.py
@@ -1,27 +1,28 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
-# Create your models here.
+
 
 class Tag(models.Model):
-	word = models.CharField(max_length=50)
-	slug = models.CharField(max_length=100)
+    word = models.CharField(max_length=50)
+    slug = models.CharField(max_length=100)
 
-	def __str__(self):
-		return self.slug
+    def __str__(self):
+        return self.slug
+
 
 class Movement(models.Model):
-	CURRENCY_CHOICES = (
+    CURRENCY_CHOICES = (
         ('EUR', 'EUR'),
         ('USD', 'USD')
     )
-	owner = models.ForeignKey(settings.AUTH_USER_MODEL)
-	tag = models.ManyToManyField(Tag)#, related_name='movements')
-	date_pub = models.DateField(default=timezone.now, verbose_name = 'Data inserimento') 
-	text = models.CharField(max_length=200, verbose_name = 'Descrizione')
-	amount = models.DecimalField(max_digits = 10, decimal_places = 2)
-	currency = models.CharField(max_length=3, null=False, blank=False, choices=CURRENCY_CHOICES)
-	#amount = MoneyField(decimal_places=2, max_digits=12, currency='EUR')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    tag = models.ManyToManyField(Tag)  # , related_name='movements')
+    date_pub = models.DateField(default=timezone.now, verbose_name='Data inserimento')
+    text = models.CharField(max_length=200, verbose_name='Descrizione')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=3, null=False, blank=False, choices=CURRENCY_CHOICES)
+    # amount = MoneyField(decimal_places=2, max_digits=12, currency='EUR')
 
-	def __str__(self):
-		return self.text
+    def __str__(self):
+        return self.text
